24575353a88b5c3f8691e1a38b3a407802a0dfb9
Leonid <firefoxwolf0824@gmail.com>
2025-10-04 21:04:48 +0300

    chore: фикс vite конфига для деплоя

--- vite.config.ts
+++ vite.config.ts
@@ -26,5 +26,6 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    allowedHosts: ['walrus-notes-y15d.onrender.com', 'localhost', '127.0.0.1'],
   },
 });
