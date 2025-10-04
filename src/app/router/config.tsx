8ee30134db14f17434c93faf06324c42a3b1dbe7
Leonid <firefoxwolf0824@gmail.com>
2025-10-04 20:32:57 +0300

    fix(router): добавлен /* урл

--- src/app/router/config.tsx
+++ src/app/router/config.tsx
@@ -1,17 +1,15 @@
-import { MainPage } from 'pages';
-import { AuthPage } from 'pages';
-import { DashBoardPage } from 'pages';
-import { NotFoundPage } from 'pages';
+import { AuthPage, DashBoardPage, MainPage, NotFoundPage } from 'pages';
 
 export const AppRoutes = {
   MAIN: '/',
   AUTH: '/auth',
   DASHBOARD: '/dashboard',
+  NOT_FOUND: '*',
 };
 
 export const appRoutesConfig = [
   { path: AppRoutes.MAIN, element: <MainPage /> },
   { path: AppRoutes.AUTH, element: <AuthPage /> },
   { path: AppRoutes.DASHBOARD, element: <DashBoardPage /> },
-  { path: '*', element: <NotFoundPage /> },
+  { path: AppRoutes.NOT_FOUND, element: <NotFoundPage /> },
 ];
