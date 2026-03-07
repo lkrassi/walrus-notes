e2d7d647b5d5bdc2c992c1f11811dfe136a98b6c
lkrassi <lkrassi@mail.ru>
2026-03-07 17:40:06 +0700

    refactor(layout): tighten public exports

--- src/features/layout/index.ts
+++ src/features/layout/index.ts
@@ -1,1 +1,3 @@
-export * from './ui/components';
+export { CreateLayoutForm } from './ui/components/CreateLayoutForm';
+export { DeleteLayoutForm } from './ui/components/DeleteLayoutForm';
+export { UpdateLayoutForm } from './ui/components/UpdateLayoutForm';
--- src/features/layout/ui/components/index.ts
+++ src/features/layout/ui/components/index.ts
@@ -1,4 +1,3 @@
 export { CreateLayoutForm } from './CreateLayoutForm';
-export { DeleteLayout } from './DeleteLayout';
 export { DeleteLayoutForm } from './DeleteLayoutForm';
 export { UpdateLayoutForm } from './UpdateLayoutForm';
