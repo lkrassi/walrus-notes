5519eadaa9371e517ae9b93dc03d245c21cd7bff
lkrassi <lkrassi@mail.ru>
2026-04-01 14:46:36 +0700

    fix: - лишний блок

--- src/pages/first/ui/FirstPage.tsx
+++ src/pages/first/ui/FirstPage.tsx
@@ -1,12 +1,10 @@
 import { PublicHeader } from '@/widgets/ui';
-import { HeroBlock } from './components/HeroBlock';
 import { First } from './First';
 
 export const FirstPage = () => {
   return (
     <>
       <PublicHeader />
-      <HeroBlock />
       <First />
     </>
   );
